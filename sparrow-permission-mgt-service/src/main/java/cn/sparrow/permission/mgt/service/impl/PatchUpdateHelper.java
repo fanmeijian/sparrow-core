319319830e67fc8989fe63ce6045211b22f84a33
Fan Sword <fanmeijian@gmail.com>
2022-03-20 16:44:53 +0800

    修复更新的bug

--- sparrow-permission-mgt-service/src/main/java/cn/sparrow/permission/mgt/service/impl/PatchUpdateHelper.java
+++ sparrow-permission-mgt-service/src/main/java/cn/sparrow/permission/mgt/service/impl/PatchUpdateHelper.java
@@ -13,30 +13,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class PatchUpdateHelper {
 
 	public static void merge(Object source, Map<String, Object> map) {
-		try {
-			System.out.println(new ObjectMapper().writeValueAsString(map));
-			Object object = new ObjectMapper().readValue(new ObjectMapper().writeValueAsString(map), source.getClass());
-			map.entrySet().forEach(f -> {
-				Field field = ReflectionUtils.findField(source.getClass(), f.getKey());
-				Method method = ReflectionUtils.findMethod(source.getClass(),
-						"set" + StringUtils.capitalize(f.getKey()), field.getType());
-				Method getMethod = ReflectionUtils
-						.findMethod(object.getClass(),
-						"get" + StringUtils.capitalize(f.getKey()));
-				// jackson反序列化时读取不到Boolean值
-				if(field.getType().equals(Boolean.class)) {
-					ReflectionUtils.invokeMethod(method, source, f.getValue());
-				}else {
-					ReflectionUtils.invokeMethod(method, source, ReflectionUtils.invokeMethod(getMethod, object));
-				}
-			});
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		map.entrySet().forEach(f -> {
+			Field field = ReflectionUtils.findField(source.getClass(), f.getKey());
+			Method setMethod = ReflectionUtils.findMethod(source.getClass(), "set" + StringUtils.capitalize(f.getKey()),
+					field.getType());
+			if (field.getType().equals(Boolean.class)) {
+				ReflectionUtils.invokeMethod(setMethod, source, Boolean.valueOf(f.getValue().toString()));
+			} else {
+				ReflectionUtils.invokeMethod(setMethod, source, f.getValue());
+			}
+		});
 
 	}
-	
+
 	public static void merge(Object source, Map<String, Object> map, Class<?> class1) {
 		try {
 			System.out.println(new ObjectMapper().writeValueAsString(map));
@@ -45,8 +34,7 @@ public class PatchUpdateHelper {
 				Field field = ReflectionUtils.findField(source.getClass(), f.getKey());
 				Method method = ReflectionUtils.findMethod(source.getClass(),
 						"set" + StringUtils.capitalize(f.getKey()), field.getType());
-				Method getMethod = ReflectionUtils
-						.findMethod(object.getClass(),
+				Method getMethod = ReflectionUtils.findMethod(object.getClass(),
 						"get" + StringUtils.capitalize(f.getKey()));
 				ReflectionUtils.invokeMethod(method, source, ReflectionUtils.invokeMethod(getMethod, object));
 			});
